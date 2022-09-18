76b28b78d5db0d1f3706970ae8e436b609314460
Matthew-Cravinhos <matthewscravinhos@gmail.com>
2022-09-18 10:44:24 -0500

    looping exercise

---
+++ looping_structures.py
@@ -0,0 +1,15 @@
+from cgi import test
+
+
+testNum = int(input("Enter an integer to test for prime: "))
+
+if testNum > 1:
+    for i in range(2, testNum):
+        if (testNum % i) == 0:
+            print(testNum, " is not a prime number")
+            print(i," times ", testNum//i, "is ", testNum)
+            break
+elif:
+    print(testNum, " is a prime number")
+else:
+    print("You must enter an integer greater than 1.")
